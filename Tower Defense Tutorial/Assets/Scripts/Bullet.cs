ce5a4accf34f956c394119c00cc5c572e79f7912
Brandon Guzman <brandon.j.guzman.22@dartmouth.edu>
2021-09-29 15:03:10 -0400

    shooting with light

--- Tower Defense Tutorial/Assets/Scripts/Bullet.cs
+++ Tower Defense Tutorial/Assets/Scripts/Bullet.cs
@@ -7,6 +7,7 @@ public class Bullet : MonoBehaviour
     private Transform target;
     public float speed = 70f;
     public GameObject impactEffect;
+    public float explosionRadius = 0;
 
     public void Seek (Transform _target){
         target = _target;
@@ -33,13 +34,43 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget(){
         GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+        Destroy(effectIns, 5f);
+
+        if (explosionRadius > 0f){
+
+            Explode();
+        }else{
+            Damage(target);
+
+        }
+
+
 
         Destroy(gameObject);
+    }
+
+    void Explode(){
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.tag == "Enemy"){
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy){
+        Destroy(enemy.gameObject);
+
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
